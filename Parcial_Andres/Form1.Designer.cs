a0f1b16c4e52d467f2358c6b7850d4ec389c55a1
essneider07 <essneidervalderrama@gmail.com>
2021-09-07 12:13:31 -0500

    Agregar archivos de proyecto.

---
+++ Parcial_Andres/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Parcial_Andres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPanaderia = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPanaderia
+            // 
+            this.lblPanaderia.AutoSize = true;
+            this.lblPanaderia.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPanaderia.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblPanaderia.Location = new System.Drawing.Point(255, 43);
+            this.lblPanaderia.Name = "lblPanaderia";
+            this.lblPanaderia.Size = new System.Drawing.Size(236, 29);
+            this.lblPanaderia.TabIndex = 0;
+            this.lblPanaderia.Text = "Panaderia OnlyPans";
+            this.lblPanaderia.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(147, 118);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(97, 33);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblPanaderia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPanaderia;
+        private System.Windows.Forms.Label lblUsuario;
+    }
+}
+
